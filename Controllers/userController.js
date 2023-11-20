f329ed3d4329d2c16b13e8273b4365401e5cf754
Navaneeth Prem <139307935+navaneethprem@users.noreply.github.com>
2023-11-20 11:45:03 +0530

    second commit

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,4 +1,5 @@
 const users = require('../Models/userSchema')
+const jwt = require('jsonwebtoken') 
 
 //  register
 exports.register = async(req,res)=>{
@@ -19,5 +20,25 @@ exports.register = async(req,res)=>{
     }
     catch(err){
         res.status(401).json(`Register API Failed, Error: ${err}`)
+    }
+}
+
+// login
+
+exports.login = async (req,res)=>{
+    console.log("Inside login function");
+    const {email,password} = req.body
+    try{
+        const existingUser = await users.findOne({email,password})
+        if(existingUser){
+            const token = jwt.sign({userId:existingUser._id},"supersecretkey12345")
+            res.status(200).json({
+                existingUser,token
+            })
+        }else{
+            res.status(404).json("Email or Password is incorrect")
+        }
+    }catch (err) {
+        res.status(401).json(`LoginAPI Failed:${err}`)
     }
 }
